0f87b5204cbae2b26a40ba5ccbf80a05831a1307
snehalbharavad <snehalbharvad2210@gmail.com>
2023-08-12 10:44:05 +0530

    first commit

--- lib/services/ApiService.dart
+++ lib/services/ApiService.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart';
 import 'package:newsapi/Model/Article.dart';
 
 class ApiService{
-  final endPoint="https://newsapi.org/v2/everything?q=tesla&from=2023-06-27&sortBy=publishedAt&apiKey=d1f8c28e22874ed2beb7fc243212a190";
+  final endPoint="https://newsapi.org/v2/everything?q=apple&from=2023-08-11&to=2023-08-11&sortBy=popularity&apiKey=d1f8c28e22874ed2beb7fc243212a190";
 
   Future<List<Article>> getArticle() async {
     Response response =await get(Uri.parse(endPoint));
